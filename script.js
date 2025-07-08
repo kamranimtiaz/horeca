dd036ecec7e05f98a5e102b4eb485a3b30b3388a
kamranimtiaz <kamranimtiaz1@yahoo.com>
2025-07-09 01:11:30 +0200

    Updated the animation timeline

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function () {
   gsap.ticker.lagSmoothing(0);
 
   // Get the fold panel element
-  const foldPanel = document.querySelector(".ix_fold_panel.is-bottom");
+  const bottomFoldPanel = document.querySelector(".ix_fold_panel.is-bottom");
 
   // Get the top fold panel element
   const topFoldPanel = document.querySelector(".ix_fold_panel.is-top");
@@ -34,50 +34,43 @@ document.addEventListener("DOMContentLoaded", function () {
   // Set initial styles for top panel
   gsap.set(topFoldPanel, {
     transformOrigin: "center top",
+    transformPerspective: 600,
+    transformStyle: "preserve-3d",
   });
-
-  // Create timeline for top fold panel
-  const tlTop = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".ix_fold_wrap",
-      start: "top 20%", // When bottom of trigger hits bottom of viewport
-      end: "bottom 20%", // When bottom of trigger hits 20% from top of viewport
-      scrub: 0.25,
-    },
-  });
-
-  // First phase: 0% to 65% - Scale Y and Translate Y animation
-  tlTop.to(topFoldPanel, {
-    rotationX: -30,
-  });
-
   // Set initial styles
-  gsap.set(foldPanel, {
-    // scaleY: 0.2,
-    yPercent: -54,
-    rotationX: 30,
+  gsap.set(bottomFoldPanel, {
+    transformPerspective: 600,
+    transformStyle: "preserve-3d",
     transformOrigin: "center bottom",
   });
 
-  // Create the main timeline
+  // Create timeline for top fold panel
   const tl = gsap.timeline({
     scrollTrigger: {
-      trigger: foldPanel,
-      start: "top bottom", // When bottom of trigger hits bottom of viewport
-      end: "top 20%",
-      scrub: 0, // Smooth scrubbing
+      trigger: ".ix_fold_wrap",
+      markers: true,
+      start: "top top", // When bottom of trigger hits bottom of viewport
+      end: "bottom 5%", // When bottom of trigger hits 20% from top of viewport
+      scrub: 1,
     },
   });
 
   // First phase: 0% to 65% - Scale Y and Translate Y animation
-//   tl.to(foldPanel, {
-//     scaleY: 1,
-//     yPercent: 0,
-    
-//   });
-  // Second phase: 65% to 100% - Rotate X animation
-  tl.to(foldPanel, {
-    // rotationX: 0,
-    yPercent: 0,
-  });
+  tl
+  .to(topFoldPanel, {
+    duration: 5,
+    rotationX: -10,
+  })
+  .to(bottomFoldPanel, {
+    rotationX: 15 ,
+    duration: 5,
+  }, 0)
+  .to(topFoldPanel, {
+    duration: 2.5,
+    rotationX: -30,
+  })
+  .to(bottomFoldPanel, {
+    rotationX: 0 ,
+    duration: 4
+  }, "75%");
 });
